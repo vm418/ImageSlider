9f19feeb5c7668631ca44892a4fe3364a2184890
vm418 <vm418@cam.ac.uk>
2022-10-31 13:09:51 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const container = document.querySelector('.container');
+const nxt = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+
+const pictures = [
+    './mrana.jpg);',
+    './ashoka-the-great.jpg);',
+    './baji.jpg);',
+    './raja.jpg);',
+    './samg.jpg);',
+    './shivaji-maharaj.jpg);'
+];
+
+let current = 0;
+
+function forward(){
+    if (current == 5 ){
+        current = 0;
+    }else{
+        current ++;
+    }
+    container.style.cssText = "background-image: url(" + pictures[current];
+}
+
+nxt.addEventListener('click', () => {
+    if (current == 5 ){
+        current = 0;
+    }else{
+        current ++;
+    }
+    container.style.cssText = "background-image: url(" + pictures[current];
+});
+
+prev.addEventListener('click', () => {
+    if (current == 0 ){
+        current = 5;
+    }else{
+        current -= 1;
+    }
+    container.style.cssText = "background-image: url(" + pictures[current];
+});
+
+
+const btns = document.querySelectorAll('.dot');
+const arr = [...btns];
+btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        current = arr.indexOf(btn);
+        container.style.cssText = "background-image: url(" + pictures[arr.indexOf(btn)];
+    });
+});
+
+
+setInterval(forward, 5000);
